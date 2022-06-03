2d25a1690037eb070087a6eb52390998bef5883d
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-06-04 08:35:17 +0900

    Create Beautifulsoup4_install.py

---
+++ scr/WebCrawling/Beautifulsoup4_install.py
@@ -0,0 +1,15 @@
+//pip3 or pip
+pip install beautifulsoup4
+
+
+
+//for windows vscode pip setting
+
+//vscode - termianl - cmd setting
+
+//AppData/python 경로 (python 폴더 경로 and python/Scripts 경로)
+//ex. C:\Users\사용자이름\AppData\Local\Programs\Python\Python310\Scripts
+
+//고급 시스템 설정 보기 - 고급 - 환경변수 - 2가지
+//User에 대한 사용자 변수 - path - 새로 만들기 - scripts 주소
+//시스템 변수 - path - python 폴더 주소
