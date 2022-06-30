7d05ff2fbaf061d28772b4d0099e1bf5ead216b4
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-06-30 21:58:31 +0900

    Update selenium_Edge.py

--- scr/WebCrawling/selenium/selenium_Edge.py
+++ scr/WebCrawling/selenium/selenium_Edge.py
@@ -1,6 +1,13 @@
+import time
 from selenium import webdriver
 
 url = "https://www.naver.com"
 
-browser = webdriver.Edge('C:\\Users\\kim\\Desktop\\vs\\msedgedriver.exe') #\할 경우 주석처리가 됨으로 escape sequence로 \\ 
-req = browser.get(url)
+
+browser = webdriver.Edge('C:\\Users\\kim\\Desktop\\vs\\msedgedriver.exe')
+browser.get(url)
+
+time.sleep(3) #browser.implicitly_wait(3)
+print(browser.title)
+
+browser.close()
