f0e8efb289a2ba900810ec9c70c70100bf1165e0
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-06-30 21:09:06 +0900

    Update selenium_install.py

--- scr/WebCrawling/selenium/selenium_install.py
+++ scr/WebCrawling/selenium/selenium_install.py
@@ -1,2 +1,14 @@
-//pip3 or pip
+#pip3 or pip
 pip install selenium
+
+
+#web driver
+
+#edge
+https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/?ranMID=24542&ranEAID=je6NUbpObpQ&ranSiteID=je6NUbpObpQ-EnGfFxVgWSACrkRNKNEEFQ&epi=je6NUbpObpQ-EnGfFxVgWSACrkRNKNEEFQ&irgwc=1&OCID=AID2200057_aff_7593_1243925&tduid=%28ir__zjr6zvyclokf6k3ggdd0ler1ff2xvs9a0d6e006i00%29%287593%29%281243925%29%28je6NUbpObpQ-EnGfFxVgWSACrkRNKNEEFQ%29%28%29&irclickid=_zjr6zvyclokf6k3ggdd0ler1ff2xvs9a0d6e006i00
+  
+#safari
+https://webkit.org/blog/6900/webdriver-support-in-safari-10/
+    
+#chrome
+https://sites.google.com/chromium.org/driver/
